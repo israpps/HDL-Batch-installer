01e48ff9b484a1742e37999506de6c2ab70b2b57
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-01-28 23:33:14 -0300

    `v3.4.3 - Revision 0`

---
+++ HDL-Batch-installer-SRC/_bin/playground/release.bat
@@ -0,0 +1,4 @@
+copy HDL-Batch-installer.exe ..\..\..\Release\HDL-Batch-installer.exe
+copy changes.log ..\..\..\Release\changes.log
+copy HDL-Batch-installer-x86.exe ..\..\..\Release32\HDL-Batch-installer.exe
+PAUSE
