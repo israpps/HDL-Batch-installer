d0bc101542ba8c64a23749439fb21f40b74a152f
israpps <57065102+israpps@users.noreply.github.com>
2023-09-30 11:05:06 -0300

    `v3.5.1 - Revision 2`

--- HDL-Batch-installer-SRC/version.h
+++ HDL-Batch-installer-SRC/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "11";
-	static const char MONTH[] = "08";
+	static const char DATE[] = "30";
+	static const char MONTH[] = "09";
 	static const char YEAR[] = "2023";
-	static const char UBUNTU_VERSION_STYLE[] =  "23.08";
+	static const char UBUNTU_VERSION_STYLE[] =  "23.09";
 	
 	//Software Status
 	static const char STATUS[] =  "Release Candidate";
@@ -17,13 +17,13 @@ namespace AutoVersion{
 	static const long MAJOR  = 3;
 	static const long MINOR  = 5;
 	static const long BUILD  = 1;
-	static const long REVISION  = 1;
+	static const long REVISION  = 2;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 0;
-	#define RC_FILEVERSION 3,5,1,1
-	#define RC_FILEVERSION_STRING "3, 5, 1, 1\0"
-	static const char FULLVERSION_STRING [] = "3.5.1.1";
+	#define RC_FILEVERSION 3,5,1,2
+	#define RC_FILEVERSION_STRING "3, 5, 1, 2\0"
+	static const char FULLVERSION_STRING [] = "3.5.1.2";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 99;
