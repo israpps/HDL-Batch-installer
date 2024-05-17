5318d759fe566d3652b0e67c90afc27b76b706ff
israpps <57065102+israpps@users.noreply.github.com>
2024-05-17 15:09:56 -0300

    bump verstring

--- HDL-Batch-installer-SRC/version.h
+++ HDL-Batch-installer-SRC/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "13";
-	static const char MONTH[] = "03";
+	static const char DATE[] = "17";
+	static const char MONTH[] = "05";
 	static const char YEAR[] = "2024";
-	static const char UBUNTU_VERSION_STYLE[] =  "24.03";
+	static const char UBUNTU_VERSION_STYLE[] =  "24.05";
 	
 	//Software Status
 	static const char STATUS[] =  "Release Candidate";
@@ -15,15 +15,15 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR  = 3;
-	static const long MINOR  = 5;
-	static const long BUILD  = 1;
-	static const long REVISION  = 6;
+	static const long MINOR  = 6;
+	static const long BUILD  = 0;
+	static const long REVISION  = 0;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 10;
-	#define RC_FILEVERSION 3,5,1,6
-	#define RC_FILEVERSION_STRING "3, 5, 1, 6\0"
-	static const char FULLVERSION_STRING [] = "3.5.1.6";
+	#define RC_FILEVERSION 3,6,0,0
+	#define RC_FILEVERSION_STRING "3, 6, 0, 0\0"
+	static const char FULLVERSION_STRING [] = "3.6.0.0";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 99;
