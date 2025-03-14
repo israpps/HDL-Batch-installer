772422efb87f2007bcf7821d9c152d9aa5753355
Matías Israelson <57065102+israpps@users.noreply.github.com>
2025-03-14 14:20:09 -0300

    push remaining files for release

--- HDL-Batch-installer-SRC/_bin/playground/release.bat
+++ HDL-Batch-installer-SRC/_bin/playground/release.bat
@@ -1,6 +1,4 @@
 copy HDL-Batch-installer.exe ..\..\..\Release\HDL-Batch-installer.exe
 copy libps2hdd.dll ..\..\..\Release\libps2hdd.dll
 copy changes.log ..\..\..\Release\changes.log
-copy HDL-Batch-installer-x86.exe ..\..\..\Release32\HDL-Batch-installer.exe
-copy libps2hdd-x86.dll ..\..\..\Release32\libps2hdd-x86.dll
 PAUSE
